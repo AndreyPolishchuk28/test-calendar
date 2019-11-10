7f48685c9160a5b1b2dffe031e7f17aeebd00673
Андрей Полищук <ipolishchuk94@gmail.com>
2019-11-10 12:44:53 +0200

    Updates

---
+++ precache-manifest.9a5926bbb1a76dfdd4b0eba9fa76e499.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46ad89001a12a3f6cac47ac029440850",
+    "url": "/test-calendar/index.html"
+  },
+  {
+    "revision": "49581fa520586619234d",
+    "url": "/test-calendar/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "622accef6ff7e32c6d1a",
+    "url": "/test-calendar/static/js/2.5bf99192.chunk.js"
+  },
+  {
+    "revision": "49581fa520586619234d",
+    "url": "/test-calendar/static/js/main.a7489af1.chunk.js"
+  },
+  {
+    "revision": "48ca98d955fbd886f4ca",
+    "url": "/test-calendar/static/js/runtime-main.25b50787.js"
+  }
+]);
